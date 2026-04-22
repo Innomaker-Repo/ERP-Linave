a9ee1b63ac65ff05b971b899b847c8e35c926c86
Hugo F. Saad <147430954+HDenial@users.noreply.github.com>
2026-04-22 12:27:40 +0000

    Update admin.py

--- BackEnd/ComercialApp/admin.py
+++ BackEnd/ComercialApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Cliente, Negocio, Servico, User
+from .models import Levantamento, MDO, Ativ_previstas, Materiais, Servicos_terceirizados, Observacoes_setor_orcamento, Resumo_orcamento
 
 # Register your models here.
 
